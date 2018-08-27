6fee6f31f88c6110b96feb52bdcac359094cdcfa
Nikolas Melgarejo <nikoclops@gmail.com>
2018-08-27 18:13:49 -0400

    Finished the tic tac toe project

--- app.js
+++ app.js
@@ -2,16 +2,17 @@
 
 const eventListeners = function () {
   $('.cell').on('click', function () {
-
-
+   if(state.gameOver === false) { 
+    
     let closestP = $(this).find('p');
     let closestPValue = closestP.text();
-    if (closestPValue === '\xa0') {
-      closestP.html(state.addXO);
-      state.stateXO = !state.stateXO;
+      if (closestPValue === '\xa0') {
+        closestP.html(state.addXO);
+        state.stateXO = !state.stateXO;
+      }
     }
-
   });
+  
 
   $('#new-game').on('click', function () {
     state.newGame();
@@ -19,6 +20,65 @@ const eventListeners = function () {
     eventListeners();
   });
 
+  $('.cell').on('click', function ( ) {
+    if($('#0').find("p").text() !== '\xa0' && $('#0').find("p").text() === $('#1').find("p").text() && $('#0').find("p").text() === $('#2').find("p").text()) {
+      state.gameOver = true;
+      console.log(state.gameOver);
+      $('#0').addClass("win");
+      $('#1').addClass("win");
+      $('#2').addClass("win");
+    }
+    if($('#0').find("p").text() !== '\xa0' && $('#0').find("p").text() === $('#3').find("p").text() && $('#0').find("p").text() === $('#6').find("p").text()) {
+      state.gameOver = true;
+      console.log(state.gameOver);
+      $('#0').addClass("win");
+      $('#3').addClass("win");
+      $('#6').addClass("win");
+    }
+    if($('#0').find("p").text() !== '\xa0' && $('#0').find("p").text() === $('#4').find("p").text() && $('#0').find("p").text() === $('#8').find("p").text()) {
+      state.gameOver = true;
+      console.log(state.gameOver);
+      $('#0').addClass("win");
+      $('#4').addClass("win");
+      $('#8').addClass("win");
+    }
+    if($('#3').find("p").text() !== '\xa0' && $('#3').find("p").text() === $('#4').find("p").text() && $('#3').find("p").text() === $('#5').find("p").text()) {
+      state.gameOver = true;
+      console.log(state.gameOver);
+      $('#3').addClass("win");
+      $('#4').addClass("win");
+      $('#5').addClass("win");
+    }
+    if($('#6').find("p").text() !== '\xa0' && $('#6').find("p").text() === $('#7').find("p").text() && $('#6').find("p").text() === $('#8').find("p").text()) {
+      state.gameOver = true;
+      console.log(state.gameOver);
+      $('#6').addClass("win");
+      $('#7').addClass("win");
+      $('#8').addClass("win");
+    }
+    if($('#1').find("p").text() !== '\xa0' && $('#1').find("p").text() === $('#4').find("p").text() && $('#1').find("p").text() === $('#7').find("p").text()) {
+      state.gameOver = true;
+      console.log(state.gameOver);
+      $('#1').addClass("win");
+      $('#4').addClass("win");
+      $('#7').addClass("win");
+    }
+    if($('#2').find("p").text() !== '\xa0' && $('#2').find("p").text() === $('#5').find("p").text() && $('#2').find("p").text() === $('#8').find("p").text()) {
+      state.gameOver = true;
+      console.log(state.gameOver);
+      $('#2').addClass("win");
+      $('#5').addClass("win");
+      $('#8').addClass("win");
+    }
+    if($('#2').find("p").text() !== '\xa0' && $('#2').find("p").text() === $('#4').find("p").text() && $('#2').find("p").text() === $('#6').find("p").text()) {
+      state.gameOver = true;
+      console.log(state.gameOver);
+      $('#2').addClass("win");
+      $('#4').addClass("win");
+      $('#6').addClass("win");
+    }
+
+  });
 };
 
 function render() {
@@ -26,7 +86,7 @@ function render() {
 }
 
 const state = (function () {
-
+  let gameOver = false;
   let stateXO = false;
 
   const addHTML = `<div class="row">
@@ -80,13 +140,15 @@ const state = (function () {
 
   const newGame = function () {
     this.stateXO = false;
+    state.gameOver = false;
   };
 
   return {
     addHTML,
     stateXO,
     addXO,
-    newGame
+    newGame,
+    gameOver
   };
 }());
 
